bed14b0bd6b5280b1fd24c380c7cd6f63a1ddd91
Mohit Sharma <sharmamohit82900@gmail.com>
2025-07-02 13:02:46 +0530

    created a seprate file for product details

--- mini-projects/02_amazon_clone/home.js
+++ mini-projects/02_amazon_clone/home.js
@@ -1,25 +1,4 @@
-const products = [
-  {
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    img: "./photos/athletic-cotton-socks-6-pairs.jpg",
-    rating: {
-      stars: 4.5,
-      review: 40,
-    },
-    priceCent: 1090,
-    id: "01",
-  },
-  {
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    img: "./photos/athletic-cotton-socks-6-pairs.jpg",
-    rating: {
-      stars: 4.5,
-      review: 40,
-    },
-    priceCent: 1090,
-    id: "02",
-  },
-];
+import { products } from "./product_details.js";
 
 const productContainer = document.querySelector("#product_container");
 // console.log(productContainer);
---
+++ mini-projects/02_amazon_clone/product_details.js
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    img: "./photos/athletic-cotton-socks-6-pairs.jpg",
+    rating: {
+      stars: 4.5,
+      review: 40,
+    },
+    priceCent: 1090,
+    id: "01",
+  },
+  {
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    img: "./photos/athletic-cotton-socks-6-pairs.jpg",
+    rating: {
+      stars: 4.5,
+      review: 40,
+    },
+    priceCent: 1090,
+    id: "02",
+  },
+];
